c89334607aaea67eeabe344ad90dcd3021d99c13
Drake Scott <dascott0204@gmail.com>
2017-02-18 22:17:58 +0000

    made a reuseable component

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-new Vue({
-    el: '#app',
-    data: {
-        status: 'Critical'
+Vue.component('my-cmp',{
+    data: function () {
+        return {
+            status: 'Critical'
+        };
     },
     template: '<p>Server Status: {{ status }}</p>'
+});
+
+new Vue({
+    el: '#app',
 })
